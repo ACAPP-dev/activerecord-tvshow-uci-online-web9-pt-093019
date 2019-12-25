7962abd7d215491d71b4cf264dc121682e1e834b
Andrew Capp <acapp909@gmail.com>
2019-12-25 02:43:05 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -1,9 +1,11 @@
 class CreateShows < ActiveRecord::Migration[5.2]
 
-  create_table :shows do |t|
-    t.string :name
-    t.string :network
-    t.string :day
-    t.integer :rating
+  def change
+    create_table :shows do |t|
+      t.string :name
+      t.string :network
+      t.string :day
+      t.integer :rating
+    end
   end
 end
